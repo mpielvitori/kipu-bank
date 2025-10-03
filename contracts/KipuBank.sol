ea23f18037f0c9690c373517412db698b549cb69
Martín Pielvitori <martinpielvitori@gmail.com>
2025-10-04 00:00:40 +0200

    Remove requirements from contract

--- contracts/KipuBank.sol
+++ contracts/KipuBank.sol
@@ -1,48 +1,5 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
-/*
-Características de KipuBank:
-    * Los usuarios pueden depositar tokens nativos (ETH) en una bóveda personal.
-    * Los usuarios pueden retirar fondos de su bóveda, pero solo hasta un umbral fijo por transacción, representado por una variable inmutable.
-    * El contrato aplica un límite global de depósitos (bankCap), establecido durante el despliegue.
-    * Las interacciones internas y externas deben seguir buenas prácticas de seguridad y revertir con errores personalizados claros si no se cumplen las condiciones.
-    * Se deben emitir eventos tanto en depósitos como en retiros exitosos.
-    * El contrato debe llevar un registro del número de depósitos y retiros.
-    * El contrato debe tener al menos una función externa, una privada y una de vista (view).
-
-Prácticas de seguridad a seguir:
-    * Usar errores personalizados en lugar de require strings.
-    * Respetar el patrón checks-effects-interactions y las convenciones de nombres.
-    * Usar modificadores cuando sea apropiado para validar la lógica.
-    * Manejar transferencias nativas de forma segura.
-    * Mantener las variables de estado limpias, legibles y bien comentadas.
-    * Agregar comentarios NatSpec para cada función, error y variable de estado.
-    * Aplicar convenciones de nombres adecuadas.
-
-Entregables -> Un repositorio público llamado kipu-bank que contenga:
-    * Código del contrato dentro de la carpeta /contracts. Debe incluir:
-    * Variables inmutables || constantes
-    * Variables de almacenamiento
-    * Mapping
-    * Eventos
-    * Errores personalizados
-    * Constructor
-    * Modificador
-    * Función external payable
-    * Función privada
-    * Función external view
-    * Un archivo README.md bien estructurado que incluya:
-        * Una descripción de lo que hace el contrato.
-        * Instrucciones de despliegue.
-        * Cómo interactuar con el contrato.
-
-Objetivos de aprendizaje:
-    * Aplicar conceptos básicos de Solidity.
-    * Seguir patrones de seguridad.
-    * Usar comentarios y una estructura limpia para mejorar la legibilidad y el mantenimiento del contrato.
-    * Desplegar un contrato inteligente completamente funcional en una testnet.
-    * Crear un repositorio de GitHub que documente y muestre tu proyecto.
-*/
 
 /**
  * @title KipuBank
